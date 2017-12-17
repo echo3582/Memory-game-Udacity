4f67e0b55d60a9577efccbed3d3f32dfc187f2bb
echo3582 <qianlu3582@163.com>
2017-12-17 23:45:55 +0800

    1.添加注释 2.js css html beautifier

--- js/app.js
+++ js/app.js
@@ -2,13 +2,21 @@
  * Create a list that holds all of your cards
  */
 var cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb',
-'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
+    'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'
+];
+//该数组存储即将进行匹配的卡片
 var openCards = [];
+//已匹配的卡片对数
 var matchNum = 0;
+//玩家已走的步数（匹配一次记一步）
 var count = 0;
+//开始游戏之后的秒数计时
 var second = 0;
+//计时状态
 var isCounting = false;
+//setInterval的返回值
 var timerID = null;
+//初始化星星数量
 var starNum = 3;
 /*
  * Display the cards on the page
@@ -19,7 +27,8 @@ var starNum = 3;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -32,12 +41,13 @@ function shuffle(array) {
     return array;
 }
 
+//洗牌、翻牌、抓牌
 function getHtml() {
-	shuffle(cards);
-	$(".card").each(function(index){
-        $(this).attr("class","card");  //restart的时候把牌面背过去
-		$(this).children().attr("class",cards[index]);
-	});
+    shuffle(cards);
+    $(".card").each(function(index) {
+        $(this).attr("class", "card"); //翻牌
+        $(this).children().attr("class", cards[index]);
+    });
 }
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -49,41 +59,43 @@ function getHtml() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//点击事件
 function click() {
-    $(".card").click(function(){   
+    $(".card").click(function() {
         //防止每次点击的时候计时发生紊乱
+        //第一次点击时开始计时，此后的点击均不重新计时。
         if (!isCounting) {
             timerBegin();
             isCounting = true;
         }
-
+        //翻开两张牌，第一张牌存入openCards数组，第二张牌存入openCards数组并与第一张牌进行比对。
         if (openCards.length === 0) {
             openCards.push($(this).children().attr("class"));
             $(this).addClass("open show");
-        }  
-        else if (openCards.length === 1) {
+        } else if (openCards.length === 1) {
             openCards.push($(this).children().attr("class"));
             $(this).addClass("open show");
             match(openCards);
-        }
-        else {
+        } else {
             return false;
         }
-    });    
+    });
 }
-
+//比对函数
 function match(array) {
+    //如果openCards中两个元素相同则执行比对成功函数，失败则执行比对失败函数。
     if (array[0] === array[1]) {
         matchSuccess(array[0]);
-    }
-    else {
+    } else {
         matchFailure(array);
     }
 }
-
+//比对成功函数
 function matchSuccess(classname) {
-    $(".card").each(function(){
-         if($(this).children().attr("class") === classname) {
+    //将匹配的卡片添加match类
+    $(".card").each(function() {
+        if ($(this).children().attr("class") === classname) {
             $(this).addClass("match");
         }
     });
@@ -95,95 +107,99 @@ function matchSuccess(classname) {
         congratulation();
     }
 }
-
+//比对失败函数
 function matchFailure(classname) {
+    //找到翻开的卡片并添加动画效果
     $(".card").each(function() {
-        if($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
+        if ($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
             $(this).addClass("errorShake");
-            setTimeout('$(this).attr("class","card")',1000);
         } else if ($(this).children().attr("class") === classname[1] && $(this).hasClass("open")) {
             $(this).addClass("errorShake");
-            setTimeout('$(this).attr("class","card")',1000);
         }
     });
 
-    //卡片翻转要设置延迟，不然前面的匹配失败效果就没有了。
+    //一秒之后翻牌（要设置延迟，不然第二张牌就翻不开了）
     setTimeout(function() {
         $(".card").each(function() {
-            if($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
-                $(this).attr("class","card");
+            if ($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
+                $(this).attr("class", "card");
             } else if ($(this).children().attr("class") === classname[1] && $(this).hasClass("open")) {
-                $(this).attr("class","card");
+                $(this).attr("class", "card");
             }
         });
-    },1000);
+    }, 1000);
     openCards = [];
     $(".moves").html(++count);
     countSteps();
 
 }
-
+//掉血逻辑
 function countSteps() {
-    switch(count) {
+    switch (count) {
         case 10:
-        starRemove();
-        starNum--;
-        break;
+            starRemove();
+            starNum--;
+            break;
         case 20:
-        starRemove();
-        starNum--;
-        break;
+            starRemove();
+            starNum--;
+            break;
         case 30:
-        starRemove();
-        starNum--;         
-        break;
+            starRemove();
+            starNum--;
+            break;
     }
 }
-
+//掉血
 function starRemove() {
     $(".stars li:first").remove();
-} 
-
+}
+//游戏成功
 function congratulation() {
     var star = null;
+    //停止计时
     clearInterval(timerID);
+    //获取玩家游戏时间
     var second = $(".timer").html();
+    //一颗星时star单数，一颗以上复数。
     if (starNum === 1) {
         star = ' star';
     } else {
         star = ' stars';
     }
-    $(".successInfo").text('With ' + starNum + star + ' and use ' + 
-        second + ' seconds'+ ' and ' + count + ' moves!');
+    //显示玩家游戏信息
+    $(".successInfo").text('With ' + starNum + star + ' and use ' +
+        second + ' seconds' + ' and ' + count + ' moves!');
+    //显示congratulation页面
     $(".congratulation").show();
 }
-
+//点击重置按钮
 function reset() {
     $(".restart").click(function() {
         restart();
     });
 }
-
+//点击再玩一次
 function tryAgain() {
     $(".tryAgain").click(function() {
         $(".congratulation").hide();
         restart();
-    }); 
+    });
 }
-
+//重置游戏
 function restart() {
-        getHtml(); 
-        clearInterval(timerID);
-        count = 0;
-        second = 0;
-        isCounting = false;
-        openCards = [];
-        $(".moves").html(0);
-        $(".timer").html(0); 
-        $(".stars").empty();
-        $(".stars").append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>");
+    getHtml();
+    clearInterval(timerID);
+    count = 0;
+    second = 0;
+    isCounting = false;
+    openCards = [];
+    $(".moves").html(0);
+    $(".timer").html(0);
+    $(".stars").empty();
+    $(".stars").append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>");
 }
-
+//开始计时
 function timerBegin() {
     timerID = setInterval(function() {
         second += 1;
@@ -191,6 +207,6 @@ function timerBegin() {
     }, 1000);
 }
 
-click(); 
+click();
 reset();
 tryAgain();
