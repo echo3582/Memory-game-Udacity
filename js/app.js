58088a5b56adb73503b67304a5796a3bbd4f80ad
echo3582 <qianlu3582@163.com>
2017-12-20 21:19:01 +0800

    根据审阅建议做了如下修改： 1.修复了restart之后matchNum和starNum没有重置的问题； 2.修改注释格式使其更加规范（JSDoc），感谢建议； 3.添加游戏获胜界面动画效果bounceInDown； 4.将 .timer 所在网页元素保存在外面,提高性能。（审阅建议：通常来说，查询、获取网页元素都会需要消耗不少的资源和时间。建议在一些重复的操作中，能够将网页元素保存在外部的就将其保存在外部，避免不必要的重复获取。）

--- js/app.js
+++ js/app.js
@@ -4,19 +4,22 @@
 var cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb',
     'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'
 ];
-//该数组存储即将进行匹配的卡片
+
+/**
+* @param {array} openCards - 即将进行匹配的卡片
+* @param {number} matchNum - 已匹配的卡片对数
+* @param {number} count - 玩家已走的步数（匹配一次记一步）
+* @param {number} second - 开始游戏之后的秒数计时
+* @param {boolean} isCounting - 计时状态
+* @param {string} timerID - setInterval的返回值
+* @param {number} starNum - 初始化星星数量
+*/
 var openCards = [];
-//已匹配的卡片对数
 var matchNum = 0;
-//玩家已走的步数（匹配一次记一步）
 var count = 0;
-//开始游戏之后的秒数计时
 var second = 0;
-//计时状态
 var isCounting = false;
-//setInterval的返回值
 var timerID = null;
-//初始化星星数量
 var starNum = 3;
 /*
  * Display the cards on the page
@@ -40,8 +43,9 @@ function shuffle(array) {
 
     return array;
 }
-
-//洗牌、翻牌、抓牌
+/**
+* @description 洗牌、翻牌、抓牌
+*/
 function getHtml() {
     shuffle(cards);
     $(".card").each(function(index) {
@@ -60,20 +64,28 @@ function getHtml() {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-//点击事件
+/**
+* @description 点击事件
+*/
 function click() {
     $(".card").click(function() {
-        //防止每次点击的时候计时发生紊乱
-        //第一次点击时开始计时，此后的点击均不重新计时。
+
+        /**
+        * @description 防止每次点击的时候计时发生紊乱~ 第一次点击时开始计时，此后的点击均不重新计时。
+        */
         if (!isCounting) {
             timerBegin();
             isCounting = true;
         }
-        //当牌已经翻开或者已经匹配时，不执行点击事件
+        /**
+        * @description 当牌已经翻开或者已经匹配时，不执行点击事件。
+        */
         if ($(this).hasClass("open") || $(this).hasClass("match")) {
             return;
         }
-        //翻开两张牌，第一张牌存入openCards数组，第二张牌存入openCards数组并与第一张牌进行比对。
+        /**
+        * @description 翻开两张牌，第一张牌存入openCards数组，第二张牌存入openCards数组并与第一张牌进行比对。
+        */
         if (openCards.length === 0) {
             openCards.push($(this).children().attr("class"));
             $(this).addClass("open show");
@@ -88,18 +100,22 @@ function click() {
         }
     });
 }
-//比对函数
+/**
+* @description 比对函数，如果openCards中两个元素相同则执行比对成功函数，失败则执行比对失败函数。
+* @param {array} array - openCards数组
+*/
 function match(array) {
-    //如果openCards中两个元素相同则执行比对成功函数，失败则执行比对失败函数。
     if (array[0] === array[1]) {
         matchSuccess(array[0]);
     } else { 
         matchFailure(array);
     }
 }
-//比对成功函数
+/**
+* @description 比对成功函数，将匹配的卡片添加match类。
+* @param {string} classname - openCards数组里面的元素，也就是卡片的类型。
+*/
 function matchSuccess(classname) {
-    //将匹配的卡片添加match类
     $(".card").each(function() {
         if ($(this).children().attr("class") === classname) {
             $(this).addClass("match");
@@ -111,9 +127,11 @@ function matchSuccess(classname) {
         congratulation();
     }
 }
-//比对失败函数
+/**
+* @description 比对失败函数，找到翻开的卡片并添加匹配失败的动画效果。扣牌动作延迟1s,不然第二张牌将不会被翻开。
+* @param {string} classname - openCards数组里面的元素，也就是卡片的类型。
+*/
 function matchFailure(classname) {
-    //找到翻开的卡片并添加动画效果
     $(".card").each(function() {
         if ($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
             $(this).addClass("errorShake");
@@ -122,7 +140,6 @@ function matchFailure(classname) {
         }
     });
 
-    //一秒之后翻牌（要设置延迟，不然第二张牌就翻不开了）
     setTimeout(function() {
         $(".card").each(function() {
             if ($(this).children().attr("class") === classname[0] && $(this).hasClass("open")) {
@@ -134,7 +151,9 @@ function matchFailure(classname) {
     }, 1000);
     openCards = [];
 }
-//掉血逻辑
+/**
+* @description 掉血规则
+*/
 function countSteps() {
     switch (count) {
         case 10:
@@ -151,57 +170,68 @@ function countSteps() {
             break;
     }
 }
-//掉血
+/**
+* @description 掉血操作
+*/
 function starRemove() {
     $(".stars li:first").remove();
 }
-//游戏成功
+/**
+* @description 游戏成功，停止计时，显示玩家游戏信息，动态显示congratulation页面。
+* @description 显示玩家游戏信息，优雅的模板字符串呢~ 划重点：反引号 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/template_strings
+*/
 function congratulation() {
-    var star = null;
-    //停止计时
     clearInterval(timerID);
-    //获取玩家游戏时间
     var second = $(".timer").html();
-    //显示小星星
     for (var i = starNum - 1; i >= 0; i--) {
         $(".starShow").append("<span><i class='fa fa-star'></i></span>"); 
     }
-    //显示玩家游戏信息，优雅的模板字符串呢~ 划重点：反引号 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/template_strings
     $(".successInfo").text(`With ${second} seconds and ${count} moves!`);
-    //显示congratulation页面
+    $(".textShow").addClass("animated bounceInDown");
     $(".congratulation").show();
 }
-//点击重置按钮
+/**
+* @description 点击重置按钮
+*/
 function reset() {
     $(".restart").click(function() {
         restart();
     });
 }
-//点击再玩一次
+/**
+* @description 点击再玩一次
+*/
 function tryAgain() {
     $(".tryAgain").click(function() {
         $(".congratulation").hide();
         restart();
     });
 }
-//重置游戏
+/**
+* @description 重置游戏
+*/
 function restart() {
     getHtml();
     clearInterval(timerID);
     count = 0;
     second = 0;
     isCounting = false;
+    matchNum = 0;
+    starNum = 0;
     openCards = [];
     $(".moves").html(0);
     $(".timer").html(0);
     $(".stars").empty();
     $(".stars").append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>");
 }
-//开始计时
+/**
+* @description 开始计时
+*/
+var $timer = $(".timer");   // 将 .timer 所在网页元素保存在外面
 function timerBegin() {
     timerID = setInterval(function() {
         second += 1;
-        $(".timer").html(second);
+        $timer.html(second);
     }, 1000);
 }
 
